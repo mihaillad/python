1407af4f61a4cbf9eeace0c53d05180d72e13147
mihaillad <67302058+mihaillad@users.noreply.github.com>
2020-07-01 00:49:55 +0500

    Add files via upload

--- parsAvito.py
+++ parsAvito.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 import random
 import psycopg2
+from datetime import datetime
 
 #https://www.avito.ru/perm/kvartiry/prodam/3-komnatnye/novostroyka-ASgBAQICAUSSA8YQAkDmBxSOUsoIFIRZ?cd=1&f=ASgBAQECAUSSA8YQAkDmBxSOUsoIFIRZBUXgBxd7ImZyb20iOjUxMzAsInRvIjpudWxsfeIHF3siZnJvbSI6NTE5MiwidG8iOm51bGx9hAkUeyJmcm9tIjo2MCwidG8iOjEwMH2MLhV7ImZyb20iOjExLCJ0byI6bnVsbH2QLhV7ImZyb20iOjExLCJ0byI6bnVsbH0
 URL = 'https://www.avito.ru/perm/kvartiry/prodam/3-komnatnye/novostroyka-ASgBAQICAUSSA8YQAkDmBxSOUsoIFIRZ'
@@ -38,15 +39,25 @@ def get_content(html):
         title = title_block.get_text(strip=True)
         href = HOST+title_block.get('href')
         price = item.find('span', class_='snippet-price').get_text(strip=True).replace(' ₽', '').replace(' ', '')
+        price = int(price)
         adress = item.find('span', class_='item-address__string').get_text(strip=True)
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # now = datetime.now()
 
-        ads.append({
-            'title': title,
-            'href': href,
-            'price': price,
-            'adress': adress,
 
-        })
+        #
+        # ads.append({
+        #     'title': title,
+        #     'href': href,
+        #     'price': price,
+        #     'adress': adress,
+        # })
+
+
+        list = [title,href,price,adress,now]
+        ads.append(list)
+
+
 
     return ads
 
@@ -77,20 +88,20 @@ def get_pages(html):
 
 
 def parse():
-    # text = get_html(URL)
-    text = get_page()
+    text = get_html(URL)
+    # text = get_page()
     ads = get_content(text)
-    # pages = get_pages(text)
+    pages = get_pages(text)
     # p = random.randint(2,5)
-    # for p in range(2,pages+1):
-    #     text = get_html(URL,params={'p':p})
-    #     ads.extend(get_content(text,))
-    #     print(f'Парсим страницу {p} из {pages}.')
-    # text = get_html(URL,params={'p':p})
-    # ads.extend(get_content(text,))
-    # print(f'Парсим страницу {p} из {pages}.')
+    for p in range(2,pages+1):
+        text = get_html(URL,params={'p':p})
+        ads.extend(get_content(text,))
+        print(f'Парсим страницу {p} из {pages}.')
+    text = get_html(URL,params={'p':p})
+    ads.extend(get_content(text,))
+    print(f'Парсим страницу {p} из {pages}.')
 
-    save_file(ads, FILE)
+    # save_file(ads, FILE)
     save_sql(ads)
 
     # get_detail(ads)
@@ -109,15 +120,19 @@ def save_file(items, path):
 
 def save_sql(items):
     conn = psycopg2.connect('postgres://postgres:2126829740@localhost:5432/parserdata')
-    with conn.cursor() as cursor:
-        conn.autocommit = True
-        insert  = sql.
+    cursor=conn.cursor()
 
-    # https: // khashtamov.com / ru / postgresql - python - psycopg2 /
+    for item in items:
 
-    cursor.execute('''SELECT * FROM appartments''')
-    records = cursor.fetchall()
-    print(records)
+        query = item[0],item[1],item[2],item[3],item[4]
+        query = str(query)
+        cursor.execute("INSERT INTO appartments(title,href,price,adress,time) VALUES "+query,)
+
+
+    conn.commit()
+    conn.close()
+
+
 
 
 parse()
